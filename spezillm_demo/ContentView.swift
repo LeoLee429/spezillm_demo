3b273756909823ef45789a185d43840273746bc4
LionLoin <dg6546@gmail.com>
2024-07-11 01:30:36 -0400

    Initial Commit

---
+++ spezillm_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spezillm_demo
+//
+//  Created by Leo Lee on 11/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spezillm_demo/spezillm_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  spezillm_demoApp.swift
+//  spezillm_demo
+//
+//  Created by Leo Lee on 11/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct spezillm_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
